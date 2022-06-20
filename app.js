f3ef200696150fc0bc626bad836b44917ca10c45
asadullah696 <mr.asadullah19@gmail.com>
2022-06-21 01:55:49 +0500

    Main Java File for Assignment

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var btnSubmit = document.querySelector(".btn");
 var resultDiv = document.querySelector(".result");
 
 
+
 var vowelsCounter = () => {
   var str = inputText.value;
   var counter = 0;
@@ -45,26 +46,61 @@ window.onload = () => {
 };
 
 
-function addWord(){
+// function addWord(){
   
-  var strConsonants = "";
-  var strVowel = "";
-  var length = inputText.length
+//   var strConsonants = "";
+//   var strVowel = "";
+//   var length = inputText.length
 
-  for(var i = 0; i < length ; i++){
-    if(inputText.charAt(i) == "a" || inputText.charAt[i] == "e" || inputText.charAt[i] == "i" ||
-    inputText.charAt[i] == "o" || inputText.charAt[i] == "u" || inputText.charAt[i] == "A" ||
-    inputText.charAt[i] == "E" || inputText.charAt[i] == "I" || inputText.charAt[i] == "O" ||
-    inputText.charAt[i] == "U" ){
-          inputText.push(strVowel);
+//   for(var i = 0; i < length ; i++){
+//     if(inputText.charAt(i) == "a" || inputText.charAt[i] == "e" || inputText.charAt[i] == "i" ||
+//     inputText.charAt[i] == "o" || inputText.charAt[i] == "u" || inputText.charAt[i] == "A" ||
+//     inputText.charAt[i] == "E" || inputText.charAt[i] == "I" || inputText.charAt[i] == "O" ||
+//     inputText.charAt[i] == "U" ){
+//           inputText.push(strVowel);
           
+//     }
+//     else{
+//           input.push(strConsonants);
+      
+//     }
+//   }
+
+// }
+// console.log(addWord)
+
+
+
+function addword() {
+    var userInput = document.getElementById("input").value;
+    var userValue = userInput.toLowerCase();
+    var vowels = [];
+    var consonants = [];
+    var boolean = false;
+    
+    for(i = 0; i < userValue.length; i++){
+        if(userValue[i] === "a" || userValue[i] === "e" || userValue[i] === "i" || userValue[i] === "o" || userValue[i] === "u" ){   
+            boolean = true;break;
+        }
+    }
+    if(boolean === true){
+        vowels.push(userInput);
     }
     else{
-          input.push(strConsonants);
-      
+        consonants.push(userInput);
     }
-  }
-
+    for(j = 0; j < vowels.length; j++){
+        document.getElementById("vowelwords").innerHTML += "<li>"+ vowels[j] + "</li>";
+    }
+    for(k = 0; k < consonants.length; k++){
+        document.getElementById("consonantwords").innerHTML += "<li>"+ consonants[k] + "</li>";
+    }
 }
-console.log(addWord)
+
+// function savedword(){
+//   var dataSaved = document.getElementById("input"); 
+//   var saved = "";
+//   dataSaved.push(saved);
+//   document.getElementById("showsaved").innerHTML = saved; 
+// }
 
